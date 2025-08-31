0e9969bf0a3c6096661ba5f91b24713d51568546
Akashmed <akashmed3@gmail.com>
2025-08-31 15:17:35 +0600

    upgrade to intelli j ultimate, setup mysql database, connect database in intellij, setup flyway dependency, setup different versions of migration file.

--- src/main/java/com/example/User/Service/UserServiceApplication.java
+++ src/main/java/com/example/User/Service/UserServiceApplication.java
@@ -7,10 +7,12 @@ import org.springframework.context.ApplicationContext;
 @SpringBootApplication
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
-       ApplicationContext context = SpringApplication.run(UserServiceApplication.class, args);
-       var userServices = context.getBean(UserService.class);
-       userServices.registerUser(new User(123, "two@mail.com", "pass32", "akash"));
-	}
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(UserServiceApplication.class, args);
+        var userServices = context.getBean(UserService.class);
+        userServices.registerUser(new User(123, "two@mail.com", "pass32", "akash"));
+//        System.out.println(Thread.activeCount());
+//        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
 
 }
